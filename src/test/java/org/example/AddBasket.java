c6323f5bf4d24d5f6eb81f450ee91c114a845b64
user-name <valeria.penkovskaya@gmail.com>
2022-02-13 19:06:36 +0300

    change

--- src/test/java/org/example/AddBasket.java
+++ src/test/java/org/example/AddBasket.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class AddBasket {
+
     @Test
     public void shouldAnswerWithTrue1() {
         WebDriverManager.chromedriver().setup();
@@ -18,11 +20,18 @@ public class AddBasket {
         options.addArguments("--incognito");
         options.addArguments("disable-popup-blocking");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://pudra.by/catalog/face/klapp/bb-krem-spf-gk-cuvee-prestige-klapp");
+        driver.get("http://automationpractice.com/");
 
 
-        WebElement wekElement = driver.findElement(By.cssSelector("body > div:nth-child(1) > div.wrapper-page > main > section.section.section-card-product > div > div > div:nth-child(2) > div.col-md-12.col-sm-13 > div > div > div.card-product-content-inner > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > div.col-sm-8.col-xs-7.col-btn-pay > button"));
-        wekElement.click();
+        WebElement addBasket = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[1]/div/div[2]/div[2]/a[2]/span"));
+        addBasket.click();
+            //WebElement emailInputField = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span"));
+            // emailInputField.click();
 
+            //WebElement passwordInputField = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]/span"));
+            //passwordInputField.click();
+            // passwordInputField.sendKeys("qwerty123");
+            //WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span"));
+            // loginButton.click();
+        }
     }
-}
---
+++ src/test/java/org/example/Entry.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Entry {
+    @Test
+    public void shouldAnswerWithTrue1() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://automationpractice.com/");
+
+
+        WebElement limitImputField = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
+        limitImputField.click();
+        WebElement emailInputField = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/div[1]/input"));
+        emailInputField.click();
+        emailInputField.click();
+        emailInputField.sendKeys("valeria.penkovskaya@gmail.com");
+        WebElement passwordInputField = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/div[2]/span/input"));
+        passwordInputField.click();
+        passwordInputField.sendKeys("qwerty123");
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span"));
+        loginButton.click();
+
+
+    }
+}
