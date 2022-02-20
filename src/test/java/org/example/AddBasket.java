15e4a2fec931380865b6744ec8f4083163e73ce2
user-name <valeria.penkovskaya@gmail.com>
2022-02-20 15:04:37 +0300

    change2

--- src/test/java/org/example/AddBasket.java
+++ src/test/java/org/example/AddBasket.java
@@ -2,44 +2,38 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
 
 
 public class AddBasket {
 
-
     @Test
-    public void shouldAnswerWithTrue12() {
+    public void shouldAnswerWithTrue() throws StaleElementReferenceException {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         options.addArguments("--incognito");
         options.addArguments("disable-popup-blocking");
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
         driver.get("http://automationpractice.com/");
 
+        WebElement addCard = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[1]/div/div[2]/div[2]/a[1]/span"));
+        addCard.click();
 
-                WebElement addBasket = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[1]/div/div[2]/div[2]/a[2]/span"));
+                WebElement checkOut= driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span"));
+        checkOut.click();
 
-
-                addBasket.click();
-
-                WebElement emailInputField = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a/span"));
-                emailInputField.click();
-
-                WebElement passwordInputField = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]/span"));
-                passwordInputField.click();
-                passwordInputField.sendKeys("qwerty123");
-                WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span"));
-                loginButton.click();
+                //WebElement passwordInputField = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]/span"));
+                //passwordInputField.sendKeys("qwerty123");
+                //WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span"));
+                //loginButton.click();
             }
-        }
+
+}
+
--- src/test/java/org/example/ThreadSleep.java
+++ src/test/java/org/example/ThreadSleep.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.openqa.selenium.StaleElementReferenceException;
+
 public class ThreadSleep {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws StaleElementReferenceException, InterruptedException {
         long start = System.currentTimeMillis();
         Thread.sleep(2000);
 
     }
+
 }
 
